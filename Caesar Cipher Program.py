0988e88943a8b65dc777e7e8aeb844d7bfc266f4
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2025-05-29 08:59:59 -0400

    Add files via upload

---
+++ Caesar Cipher Program.py
@@ -0,0 +1,33 @@
+import string
+
+def main():
+    
+    # Define cipher function
+    def cipher(text, shift, alphabs):
+
+        # Define shifting alphabet function
+        def shift_alphab(alphab):
+            return alphab[shift:] + alphab[:shift]
+
+        # Show the shifted alphabet
+        shifted_alphabs = tuple(map(shift_alphab, alphabs))
+
+        # Show the final alphabet
+        final_alphab = "".join(alphabs)
+
+        # Show the shifted alphabet
+        final_shifted_alphab = "".join(shifted_alphabs)
+
+        # Traslate both final alphabet and final shifted alphabet
+        table = str.maketrans(final_alphab, final_shifted_alphab)
+
+        # Return the table
+        return text.translate(table)
+
+    reg_text = input("Enter text: ")
+    print (cipher(reg_text, 9, [string.ascii_uppercase, string.ascii_lowercase]))
+    print (cipher(reg_text, -9, [string.ascii_uppercase, string.ascii_lowercase]))
+            
+    # End of Code
+
+main()
