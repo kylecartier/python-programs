426d34c70fe8103c9fb06b130d0414b5f1e1e930
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-01-04 16:13:34 -0500

    Add files via upload

---
+++ Rofi.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os
+
+# Rofi Launcher!
+
+# Prompt user to install rofi, launch rofi, change rofi theme, or uninstall rofi 
+
+command = (input("Enter the command to launch rofi or change its theme: "))
+
+# Install rofi if it's not installed  (Debian, RHEL, or Arch based distributions)
+
+if command == 'ird':
+    os.system('sudo apt install rofi')
+
+if command == 'irr':
+    os.system('sudo dnf install rofi')
+
+if command == 'ira':
+    os.system('sudo pacman -S rofi')
+
+# If installed, launch rofi
+
+if command == 'lr':
+    os.system ('rofi -show drun -show-icons')
+
+# Lauch the theme selector
+
+if command == 'rts':
+    os.system('rofi-theme-selector')
+
+# Uninstall rofi (Debian, RHEL, or Arch based distributions)
+
+if command == 'urd':
+    os.system('sudo apt remove rofi')
+
+if command == 'urr':
+    os.system('sudo dnf remove rofi')
+
+if command == 'ura':
+    os.system('sudo pacman -R rofi')
+
+# End of Code
