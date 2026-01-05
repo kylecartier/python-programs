f76469af0e4a95c35866a053fd67817c2102bf1b
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-01-05 10:14:14 -0500

    Add files via upload

--- Rofi.py
+++ Rofi.py
@@ -4,9 +4,27 @@ import os
 
 # Rofi Launcher!
 
-# Prompt user to install rofi, launch rofi, change rofi theme, or uninstall rofi 
+# What do you want to do?
 
-command = (input("Enter the command to launch rofi or change its theme: "))
+print() 
+
+os.system('echo What would you like to do?')
+
+print()
+
+# Choose command(s) to execute 
+
+os.system('\necho Choose a command:')
+
+print('-----------------')
+
+# Lists options for commands or exit
+
+os.system('\necho ird: install rofi on debian based systems; \necho irr: install rofi on rhel based systems; \necho ira: install rofi on arch based systems; \necho lr: launch rofi; \necho rts: rofi theme selector; \necho urd: uninstall rofi on debian based systems; \necho urr: uninstall rofi on rhel based systems; \necho ura: uninstall rofi on arch based systems')
+
+# Prompt user to install rofi, launch rofi, change rofi theme, or uninstall rofi
+
+command = input('\nEnter a command: ')
 
 # Install rofi if it's not installed  (Debian, RHEL, or Arch based distributions)
 
@@ -39,5 +57,8 @@ if command == 'urr':
 
 if command == 'ura':
     os.system('sudo pacman -R rofi')
+    
+else:
+    print('\nHave a good day!')
 
 # End of Code
