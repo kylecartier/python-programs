d43e59fcf6bfb4270c7a350f043f7f99544eeef4
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-01-07 11:16:45 -0500

    Modified and moved comments

--- Application GUI (MacOS).py
+++ Application GUI (MacOS).py
@@ -5,22 +5,27 @@ from tkmacosx import Button
 
 # Disclaimer - Does not run optimally in fullscreen as of yet
 
-# Write Root
+# Make root variable
+
 root = tk.Tk()
 
 # Make a Window and its characterisiics!
+
 root.title("Applications GUI")
 canvas = tk.Canvas(root, height=600, width=760, bg="#3c3836", borderwidth=0, highlightthickness=0)
 canvas.pack()
 
 # Disables the maximuze button
+
 root.resizable(False, False)
 
 # Make a frame
+
 frame = tk.Frame(root, bg="#3c3836")
 frame.place(relheight=0.95, relwidth=0.92, relx=0.04, rely=0.04)
 
 # Add any labels
+
 Label(frame, text="Welcome!", font=14, bg="#3c3836", fg="#928374").grid(row=1, column=3, padx=5, pady=4)
 Label(frame, text="by Kyle Cartier", bg="#3c3836", fg="#928374").grid(row=2, column=3, padx=3, pady=1)
 Label(frame, text="-------------", bg="#3c3836", fg="#928374").grid(row=3, column=2, padx=3, pady=1)
@@ -34,6 +39,7 @@ Label(frame, text="<-", bg="#3c3836", fg="#928374").grid(row=10, column=4, padx=
 Label(frame, text="Click Exit to close the app", bg="#3c3836", fg="#928374").grid(row=11, column=3, padx=3, pady=1)
 
 # Define functions for buttons
+
 def open_file(): 
     os.system('open /System/Library/CoreServices/Finder.app')
 
@@ -76,6 +82,7 @@ def Exit():
 # Buttons!
   
 # Left Side
+
 button_1 = Button(frame, text="Files", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_file)
 button_1.grid(row=4, column=1, padx=10, pady=10)
 
@@ -95,6 +102,7 @@ button_6 = Button(frame, text="Microsoft Excel", padx=8, pady=10, fg="#282828", 
 button_6.grid(row=9, column=1, padx=10, pady=10)
 
 # Right Side
+
 button_7 = Button(frame, text="Calendar", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_calendar)
 button_7.grid(row=4, column=5, padx=10, pady=10)
 
@@ -114,10 +122,12 @@ button_12 = Button(frame, text="Terminal", padx=25, pady=10, fg="#282828", bg="#
 button_12.grid(row=9, column=5, padx=10, pady=10)
 
 # Exit Button (In the middle on the bottom)
+
 exit_button = Button(frame, text="Exit", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=Exit)
 exit_button.grid(row=10, column=3, padx=10, pady=10)
 
 root.mainloop()
 
 # End of code
+
 
