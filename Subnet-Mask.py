94afa74f5cd778af124f565448c839507418de22
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-01-09 10:40:47 -0500

    Add files via upload

--- Subnet-Mask.py
+++ Subnet-Mask.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
-# Subnet Mask!
+# Subnet Mask
 
 first_octet = input("Enter the first octet: ")
 second_octet = input("Enter the second octet: ")
 third_octet = input("Enter the third octet: ")
 fourth_octet = input("Enter the fourth octet: ")
 
-# Numbers for subnetting
+# Numbers for the subnet mask
 
-networking_numbers = (128, 192, 224, 240, 248, 252, 254, 255)
+octet_numbers = (128, 192, 224, 240, 248, 252, 254, 255)
+finished_mask = (255, )
+zero = (0, )
 
 # If staatements for subnet classes
 
@@ -17,12 +19,15 @@ networking_numbers = (128, 192, 224, 240, 248, 252, 254, 255)
 
 try:
     first_octet = int(first_octet)
+    second_octet = int(second_octet)
+    third_octet = int(third_octet)
+    fourth_octet = int(fourth_octet)
 
-    if first_octet in networking_numbers:
-        if second_octet == '':
-            if third_octet == '':
-                if fourth_octet == '':
-                    subnet_mask_a = (f'{first_octet}.0.0.0')
+    if first_octet in octet_numbers:
+        if second_octet in zero:
+            if third_octet in zero:
+                if fourth_octet in zero:
+                    subnet_mask_a = (f'{first_octet}.{second_octet}.{third_octet}.{fourth_octet}')
                     result_a = (f'{subnet_mask_a} is a class A subnet.')
                     print(result_a)
                     print("This subnet mask is used with ip addresses that range from 1 to 127.")
@@ -33,11 +38,15 @@ except:
 # Class B Subnet
 
 try:
+    first_octet = int(first_octet)
     second_octet = int(second_octet)
+    third_octet = int(third_octet)
+    fourth_octet = int(fourth_octet)
 
-    if second_octet in networking_numbers:
-            if third_octet == '':
-                if fourth_octet == '':
+    if first_octet in finished_mask:
+        if second_octet in octet_numbers:
+            if third_octet in zero:
+                if fourth_octet in zero:
                     subnet_mask_b = (f'{first_octet}.{second_octet}.0.0')
                     result_b = (f'{subnet_mask_b} is a class B subnet.')
                     print(result_b)
@@ -48,11 +57,16 @@ except:
 # Class C Subnet
 
 try:
+    first_octet = int(first_octet)
+    second_octet = int(second_octet)
     third_octet = int(third_octet)
+    fourth_octet = int(fourth_octet)
 
-    if third_octet in networking_numbers:
-                if fourth_octet == "":
-                    subnet_mask_c = (f'{first_octet}.{second_octet}.{third_octet}.0')
+    if first_octet in finished_mask:
+        if second_octet in finished_mask:
+            if third_octet in octet_numbers:
+                if fourth_octet in zero:
+                    subnet_mask_c = (f'{first_octet}.{second_octet}.{third_octet}.{fourth_octet}')
                     result_c = (f'{subnet_mask_c} is a class C subnet.')
                     print(result_c)
                     print("This subnet mask is used with ip addresses that range from 192 to 223.")
@@ -60,6 +74,3 @@ except:
     exit(0)
 
 # End of Code
-
-
-
