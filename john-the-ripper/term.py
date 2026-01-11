35edf58d4d3f5aee8771501cd08971d933b3efae
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-01-11 09:22:24 -0500

    Uploaded file to folder

---
+++ john-the-ripper/term.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+
+# Enter the word to execute the program
+
+word = input("Enter a word: ")
+
+if word == "h@ck3r!":
+    os.system('./jr.py') 
+else:
+    print()
+    print("Have a good day!")
+
+# End of code
