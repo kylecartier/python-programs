28eb6cea7051a388d28dff79f93393db5dd263e7
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-02-06 07:39:51 -0500

    Updated look and feel

--- application-gui-macos.py
+++ application-gui-macos.py
@@ -12,7 +12,7 @@ root = tk.Tk()
 # Make a Window and its characterisiics!
 
 root.title("Applications GUI")
-canvas = tk.Canvas(root, height=600, width=760, bg="#3c3836", borderwidth=0, highlightthickness=0)
+canvas = tk.Canvas(root, height=600, width=760, bg="#1d1d1d", borderwidth=0, highlightthickness=0)
 canvas.pack()
 
 # Disables the maximuze button
@@ -21,22 +21,22 @@ root.resizable(False, False)
 
 # Make a frame
 
-frame = tk.Frame(root, bg="#3c3836")
+frame = tk.Frame(root, bg="#1d1d1d")
 frame.place(relheight=0.95, relwidth=0.92, relx=0.04, rely=0.04)
 
 # Add any labels
 
-Label(frame, text="Welcome!", font=14, bg="#3c3836", fg="#928374").grid(row=1, column=3, padx=5, pady=4)
-Label(frame, text="by Kyle Cartier", bg="#3c3836", fg="#928374").grid(row=2, column=3, padx=3, pady=1)
-Label(frame, text="-------------", bg="#3c3836", fg="#928374").grid(row=3, column=2, padx=3, pady=1)
-Label(frame, text="-------------", bg="#3c3836", fg="#928374").grid(row=3, column=3, padx=3, pady=1)
-Label(frame, text="-------------", bg="#3c3836", fg="#928374").grid(row=3, column=4, padx=3, pady=1)
-Label(frame, text="Main Apps", bg="#3c3836", fg="#928374").grid(row=3, column=1, padx=3, pady=1)
-Label(frame, text="Other Apps", bg="#3c3836", fg="#928374").grid(row=3, column=5, padx=3, pady=1)
-Label(frame, text="Other Apps", bg="#3c3836", fg="#928374").grid(row=3, column=5, padx=3, pady=1)
-Label(frame, text="->", bg="#3c3836", fg="#928374").grid(row=10, column=2, padx=3, pady=1)
-Label(frame, text="<-", bg="#3c3836", fg="#928374").grid(row=10, column=4, padx=3, pady=1)
-Label(frame, text="Click Exit to close the app", bg="#3c3836", fg="#928374").grid(row=11, column=3, padx=3, pady=1)
+Label(frame, text="Welcome!", font=14, bg="#1d1d1d", fg="#928374").grid(row=1, column=3, padx=5, pady=4)
+Label(frame, text="by Kyle Cartier", bg="#1d1d1d", fg="#928374").grid(row=2, column=3, padx=3, pady=1)
+Label(frame, text="-------------", bg="#1d1d1d", fg="#928374").grid(row=3, column=2, padx=3, pady=1)
+Label(frame, text="-------------", bg="#1d1d1d", fg="#928374").grid(row=3, column=3, padx=3, pady=1)
+Label(frame, text="-------------", bg="#1d1d1d", fg="#928374").grid(row=3, column=4, padx=3, pady=1)
+Label(frame, text="Main Apps", bg="#1d1d1d", fg="#928374").grid(row=3, column=1, padx=3, pady=1)
+Label(frame, text="Other Apps", bg="#1d1d1d", fg="#928374").grid(row=3, column=5, padx=3, pady=1)
+Label(frame, text="Other Apps", bg="#1d1d1d", fg="#928374").grid(row=3, column=5, padx=3, pady=1)
+Label(frame, text="->", bg="#1d1d1d", fg="#928374").grid(row=10, column=2, padx=3, pady=1)
+Label(frame, text="<-", bg="#1d1d1d", fg="#928374").grid(row=10, column=4, padx=3, pady=1)
+Label(frame, text="Click Exit to close the app", bg="#1d1d1d", fg="#928374").grid(row=11, column=3, padx=3, pady=1)
 
 # Define functions for buttons
 
@@ -83,47 +83,47 @@ def Exit():
   
 # Left Side
 
-button_1 = Button(frame, text="Files", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_file)
+button_1 = Button(frame, text="Files", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_file)
 button_1.grid(row=4, column=1, padx=10, pady=10)
 
-button_2 = Button(frame, text="Chrome", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_chrome)
+button_2 = Button(frame, text="Chrome", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_chrome)
 button_2.grid(row=5, column=1, padx=10, pady=10)
 
-button_3 = Button(frame, text="System Settings", padx=8, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_settings)
+button_3 = Button(frame, text="System Settings", padx=8, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_settings)
 button_3.grid(row=6, column=1, padx=10, pady=10)
 
-button_4 = Button(frame, text="App Store", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_store)
+button_4 = Button(frame, text="App Store", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_store)
 button_4.grid(row=7, column=1, padx=10, pady=10)
 
-button_5 = Button(frame, text="VLC", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_vlc)
+button_5 = Button(frame, text="VLC", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_vlc)
 button_5.grid(row=8, column=1, padx=10, pady=10)
 
-button_6 = Button(frame, text="Microsoft Excel", padx=8, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_excel)
+button_6 = Button(frame, text="Microsoft Excel", padx=8, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_excel)
 button_6.grid(row=9, column=1, padx=10, pady=10)
 
 # Right Side
 
-button_7 = Button(frame, text="Calendar", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_calendar)
+button_7 = Button(frame, text="Calendar", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_calendar)
 button_7.grid(row=4, column=5, padx=10, pady=10)
 
-button_8 = Button(frame, text="Notes", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_notepad)
+button_8 = Button(frame, text="Notes", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_notepad)
 button_8.grid(row=5, column=5, padx=10, pady=10)
 
-button_9 = Button(frame, text="Calculator", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_calculator)
+button_9 = Button(frame, text="Calculator", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_calculator)
 button_9.grid(row=6, column=5, padx=10, pady=10)
 
-button_10 = Button(frame, text="Activity Monitor", padx=8, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_taskmanager)
+button_10 = Button(frame, text="Activity Monitor", padx=8, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_taskmanager)
 button_10.grid(row=7, column=5, padx=10, pady=10)
 
-button_11 = Button(frame, text="Photos", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_photos)
+button_11 = Button(frame, text="Photos", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_photos)
 button_11.grid(row=8, column=5, padx=10, pady=10)
 
-button_12 = Button(frame, text="Terminal", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=open_cmd)
+button_12 = Button(frame, text="Terminal", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=open_cmd)
 button_12.grid(row=9, column=5, padx=10, pady=10)
 
 # Exit Button (In the middle on the bottom)
 
-exit_button = Button(frame, text="Exit", padx=25, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", borderless=1, focuscolor="", command=Exit)
+exit_button = Button(frame, text="Exit", padx=25, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", borderless=1, focuscolor="", command=Exit)
 exit_button.grid(row=10, column=3, padx=10, pady=10)
 
 root.mainloop()
--- application-gui-windows.py
+++ application-gui-windows.py
@@ -11,7 +11,7 @@ root = tk.Tk()
 # Make a Window and its characterisiics!
 
 root.title("Applications GUI")
-canvas = tk.Canvas(root, height=625, width=675, bg="#3c3836", borderwidth=0, highlightthickness=0)
+canvas = tk.Canvas(root, height=625, width=675, bg="#1d1d1d", borderwidth=0, highlightthickness=0)
 canvas.pack()
 
 # Disables the maximuze button
@@ -20,22 +20,22 @@ root.resizable(False, False)
 
 # Make a frame
 
-frame = tk.Frame(root, bg="#3c3836")
+frame = tk.Frame(root, bg="#1d1d1d")
 frame.place(relheight=0.95, relwidth=0.92, relx=0.04, rely=0.04)
 
 # Add any labels
 
-Label(frame, text="Welcome!", font=14, bg="#3c3836", fg="#928374").grid(row=1, column=3, padx=5, pady=4)
-Label(frame, text="by Kyle Cartier", bg="#3c3836", fg="#928374").grid(row=2, column=3, padx=3, pady=1)
-Label(frame, text="-------------", bg="#3c3836", fg="#928374").grid(row=3, column=2, padx=3, pady=1)
-Label(frame, text="-------------", bg="#3c3836", fg="#928374").grid(row=3, column=3, padx=3, pady=1)
-Label(frame, text="-------------", bg="#3c3836", fg="#928374").grid(row=3, column=4, padx=3, pady=1)
-Label(frame, text="Main Apps", bg="#3c3836", fg="#928374").grid(row=3, column=1, padx=3, pady=1)
-Label(frame, text="Other Apps", bg="#3c3836", fg="#928374").grid(row=3, column=5, padx=3, pady=1)
-Label(frame, text="Other Apps", bg="#3c3836", fg="#928374").grid(row=3, column=5, padx=3, pady=1)
-Label(frame, text="->", bg="#3c3836", fg="#928374").grid(row=10, column=2, padx=3, pady=1)
-Label(frame, text="<-", bg="#3c3836", fg="#928374").grid(row=10, column=4, padx=3, pady=1)
-Label(frame, text="Click Exit to close the app", bg="#3c3836", fg="#928374").grid(row=11, column=3, padx=3, pady=1)
+Label(frame, text="Welcome!", font=14, bg="#1d1d1d", fg="#555555").grid(row=1, column=3, padx=5, pady=4)
+Label(frame, text="by Kyle Cartier", bg="#1d1d1d", fg="#555555").grid(row=2, column=3, padx=3, pady=1)
+Label(frame, text="-------------", bg="#1d1d1d", fg="#555555").grid(row=3, column=2, padx=3, pady=1)
+Label(frame, text="-------------", bg="#1d1d1d", fg="#555555").grid(row=3, column=3, padx=3, pady=1)
+Label(frame, text="-------------", bg="#1d1d1d", fg="#555555").grid(row=3, column=4, padx=3, pady=1)
+Label(frame, text="Main Apps", bg="#1d1d1d", fg="#555555").grid(row=3, column=1, padx=3, pady=1)
+Label(frame, text="Other Apps", bg="#1d1d1d", fg="#555555").grid(row=3, column=5, padx=3, pady=1)
+Label(frame, text="Other Apps", bg="#1d1d1d", fg="#555555").grid(row=3, column=5, padx=3, pady=1)
+Label(frame, text="->", bg="#1d1d1d", fg="#555555").grid(row=10, column=2, padx=3, pady=1)
+Label(frame, text="<-", bg="#1d1d1d", fg="#555555").grid(row=10, column=4, padx=3, pady=1)
+Label(frame, text="Click Exit to close the app", bg="#1d1d1d", fg="#555555").grid(row=11, column=3, padx=3, pady=1)
 
 # Define functions for buttons
 
@@ -82,47 +82,47 @@ def Exit():
   
 # Left Side
 
-button_1 = tk.Button(frame, text="Files", padx=43, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_file)
+button_1 = tk.Button(frame, text="Files", padx=43, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_file)
 button_1.grid(row=4, column=1, padx=10, pady=10)
 
-button_2 = tk.Button(frame, text="Chrome", padx=34, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_chrome)
+button_2 = tk.Button(frame, text="Chrome", padx=34, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_chrome)
 button_2.grid(row=5, column=1, padx=10, pady=10)
 
-button_3 = tk.Button(frame, text="Settings", padx=34, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_settings)
+button_3 = tk.Button(frame, text="Settings", padx=34, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_settings)
 button_3.grid(row=6, column=1, padx=10, pady=10)
 
-button_4 = tk.Button(frame, text="Microsoft Store", padx=15, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_store)
+button_4 = tk.Button(frame, text="Microsoft Store", padx=15, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_store)
 button_4.grid(row=7, column=1, padx=10, pady=10)
 
-button_5 = tk.Button(frame, text="VLC", padx=50, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_vlc)
+button_5 = tk.Button(frame, text="VLC", padx=50, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_vlc)
 button_5.grid(row=8, column=1, padx=10, pady=10)
 
-button_6 = tk.Button(frame, text="Microsoft Excel", padx=18, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_excel)
+button_6 = tk.Button(frame, text="Microsoft Excel", padx=18, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_excel)
 button_6.grid(row=9, column=1, padx=10, pady=10)
 
 # Right Side
 
-button_7 = tk.Button(frame, text="Calendar", padx=34, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_calendar)
+button_7 = tk.Button(frame, text="Calendar", padx=34, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_calendar)
 button_7.grid(row=4, column=5, padx=10, pady=10)
 
-button_8 = tk.Button(frame, text="Notepad", padx=36, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_notepad)
+button_8 = tk.Button(frame, text="Notepad", padx=36, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_notepad)
 button_8.grid(row=5, column=5, padx=10, pady=10)
 
-button_9 = tk.Button(frame, text="Calculator", padx=32, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_calculator)
+button_9 = tk.Button(frame, text="Calculator", padx=32, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_calculator)
 button_9.grid(row=6, column=5, padx=10, pady=10)
 
-button_10 = tk.Button(frame, text="Task Manager", padx=20, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_taskmanager)
+button_10 = tk.Button(frame, text="Task Manager", padx=20, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_taskmanager)
 button_10.grid(row=7, column=5, padx=10, pady=10)
 
-button_11 = tk.Button(frame, text="Photos", padx=44, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=open_photos)
+button_11 = tk.Button(frame, text="Photos", padx=44, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=open_photos)
 button_11.grid(row=8, column=5, padx=10, pady=10)
 
-button_12 = tk.Button(frame, text="CMD", padx=50, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828",command=open_cmd)
+button_12 = tk.Button(frame, text="CMD", padx=50, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d",command=open_cmd)
 button_12.grid(row=9, column=5, padx=10, pady=10)
 
 # Exit Button (In the middle on the bottom)
 
-exit_button = tk.Button(frame, text="Exit", padx=50, pady=10, fg="#282828", bg="#928374", activebackground="#928374", activeforeground="#282828", command=Exit)
+exit_button = tk.Button(frame, text="Exit", padx=50, pady=10, fg="#1d1d1d", bg="#555555", activebackground="#555555", activeforeground="#1d1d1d", command=Exit)
 exit_button.grid(row=10, column=3, padx=10, pady=10)
 
 root.mainloop()
