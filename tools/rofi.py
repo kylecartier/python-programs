e1e68d8071b74b2b56045560d86d6d1f255d0c1f
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-02-19 17:31:52 -0500

    Moved to tools

---
+++ tools/rofi.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import os
+
+# Rofi Launcher!
+
+# What do you want to do?
+
+print() 
+
+os.system('echo What would you like to do?')
+
+print()
+
+# Choose command to execute 
+
+os.system('\necho Choose a command:')
+
+print('-----------------')
+
+# Lists options for commands or exit
+
+os.system('\necho ird: install rofi on debian based systems; \necho irr: install rofi on rhel based systems; \necho ira: install rofi on arch based systems; \necho rts: launch rofi theme selector; \necho lra: launch rofi showing all applications on the system; \necho lrf: launch rofi showing all files on the system; \necho lrw: launch rofi showing all open windows on the system; \necho urd: uninstall rofi on debian based systems; \necho urr: uninstall rofi on rhel based systems; \necho ura: uninstall rofi on arch based systems')
+
+# Prompt user to install rofi, launch rofi for applications, files, and any open windows, change rofi theme, or uninstall rofi
+
+command = input('\nEnter a command: ')
+
+# Install rofi if it's not installed  (Debian, RHEL, or Arch based distributions)
+
+if command == 'ird':
+    os.system('sudo apt install rofi -y')
+
+if command == 'irr':
+    os.system('sudo dnf install rofi -y')
+
+if command == 'ira':
+    os.system('sudo pacman -S rofi -y')
+    
+# Launch rofi theme selector
+
+if command == 'rts':
+    os.system('rofi-theme-selector')
+
+# Launch rofi (applications, files, and open windows on the system)
+
+if command == 'lra':
+    os.system('rofi -show drun -show-icons')
+
+if command == 'lrf':
+    os.system('rofi -show filebrowser -show-icons')
+    
+if command == 'lrw':
+    os.system('rofi -show window -show-icons')
+
+# Uninstall rofi (Debian, RHEL, or Arch based distributions)
+
+if command == 'urd':
+    os.system('sudo apt remove rofi -y')
+
+if command == 'urr':
+    os.system('sudo dnf remove rofi -y')
+
+if command == 'ura':
+    os.system('sudo pacman -Rns rofi -y')
+
+# Once the program is finished executing
+    
+else:
+    print('\nHave a good day!')
+
+# End of Code
